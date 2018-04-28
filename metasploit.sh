809f784fa8cb6b600c728485e21481447eb09993
7054company <34761088+7054company@users.noreply.github.com>
2018-04-28 18:47:43 +0530

    7054company -It is a required file for metasploit

---
+++ metasploit.sh
@@ -0,0 +1,68 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install figlet
+figlet 7054company
+echo "Loading" 
+
+apt install -y autoconf bison clang coreutils axel curl findutils git apr apr-util libffi-dev libgmp-dev libpcap-dev postgresql-dev readline-dev libsqlite-dev openssl-dev libtool libxml2-dev libxslt-dev ncurses-dev pkg-config wget make ruby-dev libgrpc-dev termux-tools ncurses-utils ncurses unzip zip tar postgresql termux-elf-cleaner
+
+apt update && apt upgrade
+echo "Downloading Metasploit "
+
+cd $HOME
+axel --alternate https://github.com/rapid7/metasploit-framework/archive/4.16.16.tar.gz
+tar -xf $HOME/metasploit-framework-4.16.16.tar.gz
+mv $HOME/metasploit-framework-4.16.16 $HOME/metasploit-framework
+rm $HOME/4.16.16.tar.gz
+cd $HOME/metasploit-framework
+sed '/rbnacl/d' -i Gemfile.lock
+sed '/rbnacl/d' -i metasploit-framework.gemspec
+gem install bundler
+sed 's|nokogiri (1.*)|nokogiri (1.8.0)|g' -i Gemfile.lock
+
+gem install nokogiri -v 1.8.0 -- --use-system-libraries
+ 
+#sed 's|grpc (.*|grpc (1.4.1)|g' -i $HOME/metasploit-framework/Gemfile.lock
+#gem unpack grpc -v 1.4.1
+#cd grpc-1.4.1
+#curl -LO http://7054.ezyro.com/msf/grpc.gemspec
+#curl -L http://7054.ezyro.com/msf/extconf.patch
+#patch -p1 < extconf.patch
+#gem build grpc.gemspec
+#gem install grpc-1.4.1.gem
+#cd ..
+#rm -r grpc-1.4.1
+
+
+cd $HOME/metasploit-framework
+bundle install -j5
+$PREFIX/bin/find -type f -executable -exec termux-fix-shebang \{\} \;
+rm ./modules/auxiliary/gather/http_pdf_authors.rb
+if [ -e $PREFIX/bin/msfconsole ];then
+	rm $PREFIX/bin/msfconsole
+fi
+if [ -e $PREFIX/bin/msfvenom ];then
+	rm $PREFIX/bin/msfvenom
+fi
+ln -s $HOME/metasploit-framework/msfconsole /data/data/com.termux/files/usr/bin/
+ln -s $HOME/metasploit-framework/msfvenom /data/data/com.termux/files/usr/bin/
+termux-elf-cleaner /data/data/com.termux/files/usr/lib/ruby/gems/2.4.0/gems/pg-0.20.0/lib/pg_ext.so
+echo "Creating database"
+
+cd $HOME/metasploit-framework/config
+curl -LO http://7054.ezyro.com/msf/database.yml
+
+mkdir -p $PREFIX/var/lib/postgresql
+initdb $PREFIX/var/lib/postgresql
+
+pg_ctl -D $PREFIX/var/lib/postgresql start
+createuser msf
+createdb msf_database
+
+echo " hello,
+Now you are able to use metasploit tools by command --> 
+              Tools list
+              1.msfconsole
+              2.msfvenom
+              3.....
+                    
+                        7054company  "
