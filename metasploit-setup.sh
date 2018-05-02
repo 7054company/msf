b15c36b681a443a548c768fa0a8a68c2c304bc47
7054company <34761088+7054company@users.noreply.github.com>
2018-05-02 14:07:29 +0530

    Update metasploit-setup.sh

--- metasploit-setup.sh
+++ metasploit-setup.sh
@@ -4,8 +4,6 @@ cd $HOME
 pkg install figlet
 figlet 7054company
 pkg install curl
-pkg install openssh
-y
 curl -LO https://raw.githubusercontent.com/7054company/msf/master/metasploit.sh
 chmod 777 metasploit.sh
 echo "Running metasploit-setup"
